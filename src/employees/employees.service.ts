c05072dc2f9eb522b0daf655e94a5fee0d36d3fc
gkmit-dikshant <dikshant@gkmit.co>
2025-12-13 17:58:57 +0530

    feat: add employee services

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,26 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Employee } from './entities/employee.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class EmployeesService {
-  create(createEmployeeDto: CreateEmployeeDto) {
-    return 'This action adds a new employee';
+  constructor(
+    @InjectRepository(Employee) private employeeRepo: Repository<Employee>,
+  ) {}
+
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    const employee = this.employeeRepo.create(createEmployeeDto);
+    return await this.employeeRepo.save(employee);
   }
 
-  findAll() {
-    return `This action returns all employees`;
+  async findAll(
+    page = 1,
+    limit = 10,
+    sort: keyof Employee = 'createdAt',
+    order: 'ASC' | 'DESC' = 'DESC',
+  ) {
+    const [data, total] = await this.employeeRepo.findAndCount({
+      order: { [sort]: order },
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    return {
+      data,
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} employee`;
+  async findOne(id: number) {
+    const employee = await this.employeeRepo.findOne({ where: { id } });
+
+    if (!employee) {
+      throw new HttpException(
+        `Employee with id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
-    return `This action updates a #${id} employee`;
+  async update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    const exist = await this.employeeRepo.preload({ id, ...updateEmployeeDto });
+    if (!exist) {
+      throw new HttpException(
+        `employee with id ${id} doesn't exists`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return exist;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} employee`;
+  async remove(id: number) {
+    const exist = await this.employeeRepo.findOne({ where: { id } });
+    if (!exist) {
+      throw new HttpException(
+        `employee with id ${id} doesn't exists`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    await this.employeeRepo.softDelete(id);
+    return null;
   }
 }
