faf1c0dc8062ee7a8aad32c982198ef72f6d6bb3
gkmit-dikshant <dikshant@gkmit.co>
2025-12-13 13:02:09 +0530

    feat: add employee and department entities

--- src/departments/entities/department.entity.ts
+++ src/departments/entities/department.entity.ts
@@ -1,1 +1,34 @@
-export class Department {}
+import { Employee } from 'src/employees/entities/employee.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('departments')
+export class Department {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true, nullable: false })
+  name: string;
+
+  @OneToMany(() => Employee, (employee) => employee.department, {
+    cascade: ['soft-remove'],
+    onDelete: 'RESTRICT',
+  })
+  employees: Employee[];
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true, default: null })
+  deletedAt: Date | null;
+}
--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,1 +1,46 @@
-export class Employee {}
+import { Department } from 'src/departments/entities/department.entity';
+import {
+  Check,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('employees')
+@Check('"salary" > 0')
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, length: 50 })
+  name: string;
+
+  @Column({ unique: true, nullable: false, length: 254 })
+  email: string;
+
+  @Column({ unique: true, nullable: false, length: 10 })
+  contact: string;
+
+  @Column({ nullable: false })
+  salary: number;
+
+  @ManyToOne(() => Department, (department) => department.employees, {
+    nullable: false,
+  })
+  @JoinColumn({ name: 'department_id', referencedColumnName: 'id' })
+  department: Department;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', nullable: true, default: null })
+  deletedAt: Date | null;
+}
