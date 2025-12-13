c0c81ee564d21eca9bdba376572f16a706ef2b25
gkmit-dikshant <dikshant@gkmit.co>
2025-12-13 13:29:05 +0530

    feat: add dtos for employee and department

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,1 +1,29 @@
-export class CreateEmployeeDto {}
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export class CreateEmployeeDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 50)
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  @Length(10, 10)
+  contact: string;
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  salary: number;
+  @Type(() => Number)
+  @IsInt()
+  @IsPositive()
+  departmentId: number;
+}
